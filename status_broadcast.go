f6e79d288ec2d82d6f09da74238a965f83c18508
hanzywaifu-coder <hanzywaifu-coder@users.noreply.github.com>
2026-09-06 02:51:17 +0100

    feat(status): add SendStatusBroadcastText & SendStatusBroadcastMedia for WhatsApp Story

---
+++ status_broadcast.go
@@ -0,0 +1,82 @@
+package meowbail
+
+import (
+	"context"
+	"fmt"
+
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
+	"go.mau.fi/whatsmeow/types"
+	"google.golang.org/protobuf/proto"
+)
+
+// SendStatusBroadcastText membuat status/story WhatsApp resmi (status@broadcast) berupa teks
+func (c *Client) SendStatusBroadcastText(ctx context.Context, text string, backgroundColorARGB uint32, font int32) error {
+	if text == "" {
+		return fmt.Errorf("teks status kosong")
+	}
+
+	if backgroundColorARGB == 0 {
+		backgroundColorARGB = 0xFF243B55 // Dongtube gradient midnight blue
+	}
+
+	msg := &waE2E.Message{
+		ExtendedTextMessage: &waE2E.ExtendedTextMessage{
+			Text:            proto.String(text),
+			BackgroundArgb:  proto.Uint32(backgroundColorARGB),
+			Font:            (*waE2E.ExtendedTextMessage_FontType)(&font),
+		},
+	}
+
+	_, err := c.Client.SendMessage(ctx, types.StatusBroadcastJID, msg)
+	return err
+}
+
+// SendStatusBroadcastMedia membuat status/story WhatsApp resmi (status@broadcast) berupa gambar atau video
+func (c *Client) SendStatusBroadcastMedia(ctx context.Context, mediaBytes []byte, isVideo bool, caption string) error {
+	if len(mediaBytes) == 0 {
+		return fmt.Errorf("media status kosong")
+	}
+
+	if isVideo {
+		uploaded, err := c.UploadMedia(ctx, mediaBytes, whatsmeow.MediaVideo)
+		if err != nil {
+			return fmt.Errorf("upload video status: %w", err)
+		}
+		msg := &waE2E.Message{
+			VideoMessage: &waE2E.VideoMessage{
+				URL:           proto.String(uploaded.URL),
+				DirectPath:    proto.String(uploaded.DirectPath),
+				MediaKey:      uploaded.MediaKey,
+				Mimetype:      proto.String("video/mp4"),
+				FileEncSHA256: uploaded.FileEncSHA256,
+				FileSHA256:    uploaded.FileSHA256,
+				FileLength:    proto.Uint64(uploaded.FileLength),
+				Caption:       proto.String(caption),
+			},
+		}
+		_, err = c.Client.SendMessage(ctx, types.StatusBroadcastJID, msg)
+		return err
+	}
+
+	uploaded, err := c.UploadMedia(ctx, mediaBytes, whatsmeow.MediaImage)
+	if err != nil {
+		return fmt.Errorf("upload image status: %w", err)
+	}
+
+	msg := &waE2E.Message{
+		ImageMessage: &waE2E.ImageMessage{
+			URL:           proto.String(uploaded.URL),
+			DirectPath:    proto.String(uploaded.DirectPath),
+			MediaKey:      uploaded.MediaKey,
+			Mimetype:      proto.String("image/jpeg"),
+			FileEncSHA256: uploaded.FileEncSHA256,
+			FileSHA256:    uploaded.FileSHA256,
+			FileLength:    proto.Uint64(uploaded.FileLength),
+			Caption:       proto.String(caption),
+		},
+	}
+
+	_, err = c.Client.SendMessage(ctx, types.StatusBroadcastJID, msg)
+	return err
+}
